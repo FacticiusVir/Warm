c38be2ca58353465d357b5ced4bf1488beb00800
Andrew Armstrong <facticiusvir@gmail.com>
2016-05-06 16:39:53 +0100

    Started refactoring Machine into new Core library to decouple from Prolog structures.

---
+++ Keeper.Warm.Core/Machine.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Keeper.Warm
+{
+    public class Machine
+    {
+        private int[] memory;
+        private int topOfHeapPointer;
+
+        public Machine(int memorySize)
+        {
+            this.memory = new int[memorySize];
+        }
+
+        private int MemAlloc(int size)
+        {
+            int result = this.topOfHeapPointer;
+
+            int newTopOfHeap = this.topOfHeapPointer + size;
+
+            if (newTopOfHeap > memory.Length)
+            {
+                throw new Exception("Out of memory");
+            }
+            else
+            {
+                this.topOfHeapPointer = newTopOfHeap;
+                return result;
+            }
+        }
+
+        public int LoadCodeBlock(Opcode[] code)
+        {
+            int codePointer = this.MemAlloc(code.Length);
+
+            for (int offset = 0; offset < code.Length; offset++)
+            {
+                this.memory[codePointer + offset] = (int)code[offset];
+            }
+
+            return codePointer;
+        }
+
+        public Thread SpawnThread(int instructionPointer, int stackSize)
+        {
+            int stackPointer = this.MemAlloc(stackSize);
+
+            return new Thread(this, instructionPointer, stackPointer);
+        }
+
+        public class Thread
+        {
+            private Machine parent;
+            private int instructionPointer;
+            private int stackPointer;
+            private int stackOffset;
+
+            internal Thread(Machine parent, int instructionPointer, int stackPointer)
+            {
+                this.parent = parent;
+                this.instructionPointer = instructionPointer;
+                this.stackPointer = stackPointer;
+                this.stackOffset = 0;
+            }
+
+            public bool Step()
+            {
+                var opcode = (Opcode)this.parent.memory[this.instructionPointer];
+
+                this.instructionPointer += GetInstructionSize(opcode);
+
+                bool succeed = true;
+
+                switch (opcode)
+                {
+                    case Opcode.LoadConstant0:
+                    case Opcode.LoadConstant1:
+                    case Opcode.LoadConstant2:
+                    case Opcode.LoadConstant3:
+                    case Opcode.LoadConstant4:
+                    case Opcode.LoadConstant5:
+                    case Opcode.LoadConstant6:
+                    case Opcode.LoadConstant7:
+                        this.PushValue((int)opcode & 7);
+                        break;
+                    case Opcode.Add:
+                        this.Add();
+                        break;
+                    default:
+                        throw new Exception("Unrecognised opcode: " + opcode);
+                }
+
+                return succeed;
+            }
+
+            private void Add()
+            {
+                int value = this.PopValue();
+
+                int operand = this.PopValue();
+
+                this.PushValue(value + operand);
+            }
+
+            private int PopValue()
+            {
+                this.stackOffset--;
+                return this.parent.memory[this.stackPointer + this.stackOffset];
+            }
+
+            private void PushValue(int value)
+            {
+                this.parent.memory[this.stackPointer + this.stackOffset] = value;
+                this.stackOffset++;
+            }
+            
+            private static int GetInstructionSize(Opcode opcode)
+            {
+                Opcode operandSizeFlag
+                    = opcode & Opcode.OperandMask;
+
+                switch (operandSizeFlag)
+                {
+                    case Opcode.NoOperand:
+                        return 1;
+                    case Opcode.Int32Operand:
+                    case Opcode.RulePointerOperand:
+                        return 2;
+                    default:
+                        throw new NotSupportedException();
+                }
+            }
+
+            public IEnumerable<int> Stack
+            {
+                get
+                {
+                    return this.parent.memory.Skip(this.stackPointer).Take(this.stackOffset);
+                }
+            }
+        }
+    }
+}
---
+++ Keeper.Warm.Core/Opcode.cs
@@ -0,0 +1,65 @@
+﻿namespace Keeper.Warm
+{
+    public enum Opcode
+    {
+        NoOperand = 0x01 << 24,
+        Int32Operand = 0x02 << 24,
+        RulePointerOperand = 0x03 << 24,
+        OperandMask = 0xFF << 24,
+
+        Halt = NoOperand | 0 << 8 | 0,
+        Duplicate = NoOperand | 0 << 8 | 1,
+        Allocate = Int32Operand | 0 << 8 | 2,
+        Deallocate = Int32Operand | 0 << 8 | 3,
+        Call = RulePointerOperand | 0 << 8 | 4,
+        Proceed = NoOperand | 0 << 8 | 5,
+        Fail = NoOperand | 0 << 8 | 10,
+        Nop = NoOperand | 0 << 8 | 12,
+        ChoicePoint = Int32Operand | 0 << 8 | 13,
+
+        LoadGlobalRegisterBase = NoOperand | 1 << 8,
+
+        LoadConstantBase = NoOperand | 3 << 8,
+        LoadConstant0 = LoadConstantBase | 0,
+        LoadConstant1 = LoadConstantBase | 1,
+        LoadConstant2 = LoadConstantBase | 2,
+        LoadConstant3 = LoadConstantBase | 3,
+        LoadConstant4 = LoadConstantBase | 4,
+        LoadConstant5 = LoadConstantBase | 5,
+        LoadConstant6 = LoadConstantBase | 6,
+        LoadConstant7 = LoadConstantBase | 7,
+        LoadConstant = Int32Operand | 3 << 8,
+
+        Load = NoOperand | 4 << 8,
+
+        Store = NoOperand | 5 << 8,
+
+        LoadLocal = Int32Operand | 6 << 8,
+
+        StoreLocal = Int32Operand | 7 << 8,
+
+        ArithmeticBase = NoOperand | 8 << 8,
+        Increment = ArithmeticBase | 0,
+        Add = ArithmeticBase | 1,
+
+        LoadArgumentAddressBase = NoOperand | 9 << 8,
+        LoadArgumentAddress0 = LoadArgumentAddressBase | 0,
+        LoadArgumentAddress1 = LoadArgumentAddressBase | 1,
+        LoadArgumentAddress2 = LoadArgumentAddressBase | 2,
+        LoadArgumentAddress3 = LoadArgumentAddressBase | 3,
+        LoadArgumentAddress4 = LoadArgumentAddressBase | 4,
+        LoadArgumentAddress5 = LoadArgumentAddressBase | 5,
+        LoadArgumentAddress6 = LoadArgumentAddressBase | 6,
+        LoadArgumentAddress7 = LoadArgumentAddressBase | 7,
+
+        BranchBase = Int32Operand | 10 << 8,
+        BranchEqual = BranchBase | 0,
+        BranchNotEqual = BranchBase | 1,
+        BranchAlways = BranchBase | 2,
+        BranchAbsolute = BranchBase | 3,
+
+        StoreGlobalRegisterBase = NoOperand | 11 << 8,
+
+        LoadLocalAddress = Int32Operand | 12 << 8,
+    }
+}
