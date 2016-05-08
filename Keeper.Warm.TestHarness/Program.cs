4f3925b4c922c32e9e5e02537e6e3c273a2f25cf
FacticiusVir <FacticiusVir@gmail.com>
2016-05-08 02:35:10 +0100

    Reworked TestHarness to load program from file & accept query inputs

--- Keeper.Warm.TestHarness/Program.cs
+++ Keeper.Warm.TestHarness/Program.cs
@@ -1,93 +1,63 @@
 using Keeper.Warm;
 using Keeper.Warm.Prolog;
 using System;
+using System.IO;
 using System.Linq;
 
 namespace Keeper.Warm
 {
     public static class Program
     {
-        // This is the classic Greek Gods program, here duplicated from Dan Tobin (see http://www.norsemathology.org/wiki/index.php?title=Dan_Tobin%27s_Prolog_Project)
-        private static string testFile = @"
-parent(cronus,hestia).
-parent(cronus,pluto).
-parent(cronus,poseidon).
-parent(cronus,zeus).
-parent(cronus,hera).
-parent(cronus,demeter).
-parent(rhea, hestia).
-parent(rhea, pluto).
-parent(rhea, poseidon).
-parent(rhea, zeus).
-parent(rhea, hera).
-parent(rhea, demeter).
-
-parent(zeus, athena).
-
-parent(zeus, ares).
-parent(zeus, hebe).
-parent(zeus, hephaestus).
-parent(hera, ares).
-parent(hera, hebe).
-parent(hera, hephaestus).
-
-parent(zeus, persephone).
-parent(demeter, persephone).
-
-
-male(cronus).
-male(pluto).
-male(poseidon).
-male(zeus).
-male(ares).
-male(hephaestus).
-female(rhea).
-female(hestia).
-female(hera).
-female(demeter).
-female(athena).
-female(hebe).
-female(persephone).
-
-
-isFather(X, Y):-
-	male(X),
-	parent(X, Y).
-
-isMother(X, Y):-
-	female(X),
-	parent(X, Y).
-
-isDaughter(X, Y):-
-	female(X),
-	parent(Y, X).
-
-isSon(X, Y):-
-	male(X),
-	parent(Y, X).
-
-isAncestor(X, Y):-
-	parent(X, Y).
-isAncestor(X, Y):-
-	parent(X, T),
-	parent(T, Y).";
+        // Included Prolog file is the classic Greek Gods program, here
+        // duplicated from Dan Tobin
+        // (see http://www.norsemathology.org/wiki/index.php?title=Dan_Tobin%27s_Prolog_Project)
 
         public static void Main(string[] args)
         {
-            var testHost = new Host();
-
-            var rules = Parser.ParseFile(testFile);
-
-            foreach(var rule in rules)
+            try
             {
-                testHost.AddRule(rule);
+                var testHost = new Host();
+
+                var rules = Parser.ParseFile(File.ReadAllText(".\\Program.plg"));
+
+                foreach (var rule in rules)
+                {
+                    testHost.AddRule(rule);
+                }
+                
+                bool isRunning = true;
+
+                do
+                {
+                    Console.Write("?");
+                    string line = Console.ReadLine();
+
+                    if(string.IsNullOrEmpty(line))
+                    {
+                        isRunning = false;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            var query = Parser.ParseQuery(line);
+
+                            RunQuery(testHost, query);
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine("Exception thrown: {0}", ex.Message);
+                        }
+                    }
+
+                } while (isRunning);
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception thrown: {0}", ex.Message);
 
-            RunQuery(testHost, Parser.ParseQuery("isAncestor(Ancestor, Descendant)"));
-
-            Console.WriteLine("Done");
-
-            Console.ReadLine();
+                Console.ReadLine();
+            }
         }
 
         private static void RunQuery(Host testHost, params CompoundTerm[] query)
